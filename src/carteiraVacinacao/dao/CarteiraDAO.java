84435cd077d06e2686d08c76c155b979e1200def
LucasSsouza10 <luccas_ssouza@hotmail.com>
2018-12-07 09:51:02 -0200

    add CarteiraDAO funcoes create read

--- src/carteiraVacinacao/dao/CarteiraDAO.java
+++ src/carteiraVacinacao/dao/CarteiraDAO.java
@@ -5,10 +5,66 @@
  */
 package carteiraVacinacao.dao;
 
+import carteiraVacinacao.bean.Carteira;
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author lucca
  */
 public class CarteiraDAO {
+    private ClienteDAO cdao;
+    private PacienteDAO pdao;
     
+    
+    public void create(Carteira c){
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+        
+        try {
+        stmt = con.prepareStatement("INSERT INTO Cliente VALUES (?, ?, ?, ?);");
+        stmt.setLong(1, c.getClienteModExterno().getCpf());
+        stmt.setInt(2, c.getPacienteModExterno().getId());
+        stmt.setInt(3, c.getQtd_carteiras());
+        stmt.setString(4, c.getVetorVacina());
+        
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Falha ao inserir carteira");
+        }finally{
+            Conexao.closeConnection(con, stmt);
+        }
+    }
+    
+    public void read(long cpf, int id){
+        Connection con = Conexao.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Carteira c = new Carteira();
+        
+        
+        try {
+            stmt = con.prepareStatement("Select * FROM carteira WHERE cpf_cliente = ? AND id_paciente = ?");
+            stmt.setLong(1, cpf);
+            stmt.setInt(2, id);
+            
+            if(rs.next()){
+                c.setClienteModExterno(cdao.readCliente(rs.getLong("cpf_cliente")));
+                c.setPacienteModExterno(pdao.readPacienteId(rs.getInt("id_paciente")));
+                c.setQtd_carteiras(rs.getInt("qtd"));
+                c.setVetorVacinas(rs.getString("vacinas"));
+            }else{
+                JOptionPane.showMessageDialog(null, "Carteira não existe");
+            }
+            
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Falha ao buscar carteira");
+        }finally{
+            Conexao.closeConnection(con, stmt);
+        }
+    }
 }

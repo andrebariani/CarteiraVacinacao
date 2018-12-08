5bf0a2e6bc63fdd7beedf4621127cedbbd84ca01
LeoNozaki <leonardo_nozaki@hotmail.com>
2018-12-08 15:24:54 -0200

    Mudancas na interface principal, alterado a interface inicial de execucao do projeto

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -4,19 +4,18 @@
  * and open the template in the editor.
  */
 package view;
-
 import java.awt.CardLayout;
 
 /**
  *
  * @author leona
  */
-public class TelaCardLayout extends javax.swing.JFrame {
+public class TelaPrincipal extends javax.swing.JFrame {
 
     /**
-     * Creates new form TelaCardLayout
+     * Creates new form TelaPrincipal
      */
-    public TelaCardLayout(){
+    public TelaPrincipal() {
         TelaLoginModal tlm = new TelaLoginModal(this,true);
         tlm.setVisible(true);
         
@@ -34,6 +33,7 @@ public class TelaCardLayout extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         tela1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         tela2 = new javax.swing.JPanel();
@@ -44,18 +44,37 @@ public class TelaCardLayout extends javax.swing.JFrame {
         sair = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Clínica Veterinária");
-        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        getContentPane().setLayout(null);
 
         jPanel1.setLayout(new java.awt.CardLayout());
 
-        tela1.setBackground(tela1.getBackground());
         tela1.setLayout(null);
 
         jButton1.setText("jButton1");
-        tela1.add(jButton1);
-        jButton1.setBounds(130, 120, 79, 25);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(1083, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(88, 88, 88))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(125, 125, 125)
+                .addComponent(jButton1)
+                .addContainerGap(420, Short.MAX_VALUE))
+        );
+
+        tela1.add(jPanel2);
+        jPanel2.setBounds(40, 40, 1250, 570);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/fundo.jpg"))); // NOI18N
         tela1.add(jLabel1);
@@ -66,9 +85,6 @@ public class TelaCardLayout extends javax.swing.JFrame {
         tela2.setBackground(new java.awt.Color(0, 0, 153));
         tela2.setLayout(null);
         jPanel1.add(tela2, "tela2");
-
-        getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 0, 1329, 797);
 
         carteiraVacinacao.setText("  Carteira de vacinação  ");
         carteiraVacinacao.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -99,19 +115,40 @@ public class TelaCardLayout extends javax.swing.JFrame {
 
         setJMenuBar(jMenuBar1);
 
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1329, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1329, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 797, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 797, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-    
-    private void modeloCarteiraClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_modeloCarteiraClicked
-        CardLayout cl = (CardLayout) jPanel1.getLayout();
-        cl.show(jPanel1, "tela2");
-    }//GEN-LAST:event_modeloCarteiraClicked
 
     private void carteiraVacinacaoClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_carteiraVacinacaoClicked
         CardLayout cl = (CardLayout) jPanel1.getLayout();
         cl.show(jPanel1, "tela1");
     }//GEN-LAST:event_carteiraVacinacaoClicked
+
+    private void modeloCarteiraClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_modeloCarteiraClicked
+        CardLayout cl = (CardLayout) jPanel1.getLayout();
+        cl.show(jPanel1, "tela2");
+    }//GEN-LAST:event_modeloCarteiraClicked
 
     private void sairMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_sairMouseClicked
         this.setVisible(false);
@@ -121,6 +158,10 @@ public class TelaCardLayout extends javax.swing.JFrame {
         CardLayout cl = (CardLayout) jPanel1.getLayout();
         cl.show(jPanel1, "tela1");
     }//GEN-LAST:event_sairMouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -139,20 +180,20 @@ public class TelaCardLayout extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaCardLayout.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaCardLayout.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaCardLayout.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaCardLayout.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-       
+
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaCardLayout().setVisible(true);
+                new TelaPrincipal().setVisible(true);
             }
         });
     }
@@ -164,6 +205,7 @@ public class TelaCardLayout extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JMenu modeloCarteira;
     private javax.swing.JMenu sair;
     private javax.swing.JPanel tela1;
